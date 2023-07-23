2357fe5415311d9244b0af800dbbca842723c726
MacAlsandair <mac.alsandair@gmail.com>
2023-07-23 17:55:52 +0200

    Add update-vehicle template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UpdateVehicleComponent } from './update-vehicle/update-vehicle.component';
+import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'vehicle-list', pathMatch: 'full'},
+  {path: 'vehicle-list', component: VehicleListComponent},
+  {path: 'update-vehicle', component: UpdateVehicleComponent},
+  {path: '**', redirectTo: 'vehicle-list'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
