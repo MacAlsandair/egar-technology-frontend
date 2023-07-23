5b0ffbf4ea896ba8b2acbdcb26ab7091b775fd15
MacAlsandair <mac.alsandair@gmail.com>
2023-07-23 16:52:21 +0200

    Create vehicle service

---
+++ src/app/vehicle.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class VehicleService {
+
+  private baseUrl = 'http://localhost:8080/api/v1/vehicles'; // replace with your server url
+
+  constructor(private http: HttpClient) { }
+
+  getAllVehicles(): Observable<Vehicle[]> {
+    return this.http.get<Vehicle[]>(`${this.baseUrl}`);
+  }
+
+  getVehicleById(id: number): Observable<Vehicle> {
+    return this.http.get<Vehicle>(`${this.baseUrl}/${id}`);
+  }
+
+  addVehicle(vehicleDTO: VehicleDTO): Observable<Vehicle> {
+    return this.http.post<Vehicle>(this.baseUrl, vehicleDTO);
+  }
+
+  updateVehicle(id: number, vehicleDTO: VehicleDTO): Observable<Vehicle> {
+    return this.http.put<Vehicle>(`${this.baseUrl}/${id}`, vehicleDTO);
+  }
+
+  searchVehicles(vehicleDTO: VehicleDTO): Observable<VehicleDTO[]> {
+    return this.http.get<VehicleDTO[]>(`${this.baseUrl}/search`, { params: vehicleDTO });
+  }
+}
